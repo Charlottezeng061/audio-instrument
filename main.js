7727e805de3aa773e72e38c4ba98bb0414a2ac61
Charlotte061 <s4160768@student.rmit.edu.au>
2026-08-12 14:48:54 +1000

    added intro dialog

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
+// browser loads html > browser loads js > open the dialog > user closes dialog >
+//  audio system loads > user clicks sound button
+// find out dialog
+const introDialog = document.getElementById("intro-dialog");
+// find the close button
+const introDialogCloseButton = document.getElementById("intro-dialog-close");
+//show the found element in oour browser console
+//console.log(introDialog);
 // find out test button
 const testbutton = document.getElementById('test-button');
 // init our synth
 const synth = new Tone.Synth().toDestination();
+
+
+////Dialog
+introDialog.showModal();
+introDialogCloseButton.addEventListener("click", function closeIntroDialog);{
+    introDialog.close();
+}
+
+//we put the whole function inside the event listener instead as its only called there
+//function closeIntroDialog{}
 
 // do something when this button is clicked
 testbutton.addEventListener("click", playNote)
